eb77d2cb95502ee448cf363b26d08b159d1631cd
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-07-26 11:48:19 -0500

    added friends info

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friends = [
+    {
+        name: "Charlie",
+        photo: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/7/78/Charlie_Kelly.jpg/revision/latest/scale-to-width-down/310?cb=20171214035211",
+        scores: [
+            2,
+            5,
+            1,
+            4,
+            2,
+            1,
+            4,
+            1,
+            2,
+            5
+
+        ]
+    },
+    {
+        name: "Dee",
+        photo: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/9/9f/Dee_Reynolds.jpg/revision/latest/scale-to-width-down/310?cb=20171214035415",
+        scores: [
+            3,
+            2,
+            4,
+            5,
+            3,
+            1,
+            1,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Dennis",
+        photo: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/4/4f/Dennis_Reynolds.jpg/revision/latest/scale-to-width-down/310?cb=20171214035357",
+        scores: [
+            2,
+            4,
+            5,
+            1,
+            3,
+            1,
+            3,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Frank",
+        photo: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/7/72/Frank_Reynolds.jpg/revision/latest/scale-to-width-down/310?cb=20171214035424",
+        scores: [
+            1,
+            2,
+            1,
+            4,
+            5,
+            1,
+            5,
+            1,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Mac",
+        photo: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/8/81/Mac_McDonald.jpg/revision/latest/scale-to-width-down/310?cb=201712140353090",
+        scores: [
+            5,
+            1,
+            3,
+            1,
+            3,
+            5,
+            4,
+            1,
+            3,
+            2
+
+        ]
+    },
+];
+
+module.exports = friends;
