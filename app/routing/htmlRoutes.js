c311178fad5caafca1405330cbfd0b9399f2fc67
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-08-12 20:25:09 -0500

    added second underscore to dirname

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,12 @@ var path = require("path");
 
 // routes
 module.exports = function (app) {
+    // if no matching route, default to home
     app.get("*", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
+    
 };
